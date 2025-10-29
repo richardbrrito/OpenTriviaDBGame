503523deca96488777ae7220e1332b0614a08014
Richard Brito <richardbrito116@gmail.com>
2025-10-29 15:23:22 -0400

    Initial Commit

---
+++ TriviaApp/TriviaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaAppApp.swift
+//  TriviaApp
+//
+//  Created by Richard Brito on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TriviaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TriviaAppTests/TriviaAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaAppTests.swift
+//  TriviaAppTests
+//
+//  Created by Richard Brito on 10/29/25.
+//
+
+import Testing
+@testable import TriviaApp
+
+struct TriviaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
